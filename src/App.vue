7c9965505b4aa7d89d22e929cb248df2ffb1d3f5
Angelo Melotti <96485963+callagan70@users.noreply.github.com>
2022-04-28 19:59:43 +0100

    Header Ok

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div>
+  <HeaderComp />
+    <main>
+
+
+
+
+
+
+
+
+
+
+    </main>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderComp from './components/HeaderComp.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
+    HeaderComp,
+   },
 }
+
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+*{padding: 0;
+  margin: 0;
+  box-sizing: border-box}
+
+  div{
+    font-family: Arial, Helvetica, sans-serif;
+  }
+
+// NOTE come installare FontAwesame
+// NOTE come si può inserire uno style dentro l'altro?
 </style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,106 @@
+<template>
+    <div id="header">
+
+            <img src="../assets/img/logo.png" alt="Logo">
+            <div class="headerUl">
+                    <div v-for="(element, index) in ListName" v-bind:key="index">
+                        <a href="#">{{element.name}}</a>
+                    </div>
+                    <div class="lens">
+                        <div>JOIN US</div>
+                        <img src="../assets/img/icons-search.svg" alt="" >
+                    </div>
+            </div>
+
+
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'HeaderComp',
+
+    data(){
+        return{
+        ListName:[ 
+            {
+                name: 'Home'
+            }, 
+            {
+                name: 'Apple'
+            }, 
+            {
+                name: 'Microsoft'
+            }, 
+            {
+                name: 'Android'
+            }, 
+            {
+                name: 'Forum'
+            }, 
+            {
+                name: 'Contact Us'
+            },] 
+       }
+       
+    },
+        
+}
+
+</script>
+
+<style scoped lang="scss">
+        #header{
+            display:flex;
+            justify-content: space-between;
+            align-items: center;
+            height: 100px;
+            width: 100%;
+            background-color: #26A9BD;
+            padding: 2em 20em 2em 20em;
+        }
+             
+         .headerUl{
+             display: flex;
+             flex-wrap: wrap;
+             justify-content: flex-start;
+             width: fit-content;
+             height: fit-content;
+         }
+    
+        .headerUl>div{
+            display: flex;
+            align-items: center;
+        }
+
+        .headerUl>div>a{
+            color: white;
+            text-decoration: none;
+            margin-right: 2em;
+        }
+
+        .lens{
+            display: flex;
+            justify-content: end;
+            align-items: center;
+            margin-left: 1em;
+        }
+
+        .lens img{
+            width: 1em;
+            color: white;
+            margin-left: 3em;
+            cursor: help;
+        }
+
+        .lens>div{
+            color: white;
+            background-color: black;
+            padding: 10px 20px 10px 20px;
+            border-radius: 10px;
+            cursor: pointer;
+        }
+
+
+</style>
